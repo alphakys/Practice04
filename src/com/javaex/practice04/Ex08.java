d6209ea4899635df5c2acf0ee8671bd481956ffc
'alphakys' <'gys1120@naver.com'>
2020-11-19 17:39:15 +0900

    11.19

--- src/com/javaex/practice04/Ex08.java
+++ src/com/javaex/practice04/Ex08.java
@@ -6,21 +6,47 @@ public class Ex08 {
 		
 		int[] lotto = new int[6];
 		
+		lotto[0] = (int)((Math.random()*7)+1);
+		lotto[1] = (int)((Math.random()*14)+8);
+		lotto[2] = (int)((Math.random()*21)+15);
+		lotto[3] = (int)((Math.random()*28)+22);
+		lotto[4] = (int)((Math.random()*35)+29);
+		lotto[5] = (int)((Math.random()*45)+36);
 		
-		for(int i=0;i<lotto.length;i++) {
-						lotto[i]=(int)((Math.random()*45)+1);
-										}
+		System.out.println(lotto[0]);
+		
+		
+		
+	int randomLotto = lotto[(int)(Math.random()*6)];
+		
+	boolean run = true;
+	/*
+	for(int i=0;i<6;i++) {
+					System.out.println(lotto[(int)(Math.random()*i)]);
+					}
+	
+	/*
+	while(run) {
+		
+			int count = 0;
+				
+				if(count6)
+				System.out.println(randomLotto);
+						
+										
+								
+				}
 		
 		
 		
 		
 		
-		
+		/*
 		for(int k : lotto) {
 			System.out.print(k+"  ");
 							}
 		
-
+*/
 	}
 
 }
